6945bfd4c59e19913a3ea3fd9646b3183e0fdc5e
verbalk <verbalk89@naver.com>
2016-06-03 17:24:56 +0900

    Delete connection.js

--- event_module/connection.js
+++
@@ -1,9 +0,0 @@
-var path				= require("path");
-
-module.exports = function(io){
-	io.sockets.on("connection",function(socket){
-		socket.session = socket.client.conn.request.session;
-		if (!socket.session || !socket.session.user) { return; } // 비로그인시 차단
-		socket.session.project = null; // 초기화
-	});
-};
